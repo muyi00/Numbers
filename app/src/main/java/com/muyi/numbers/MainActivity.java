39b755a8cfdac5913d8614c2d39d8c628028e102
253682259@qq.com <y4879153>
2018-01-18 14:51:51 +0800

    添加bmobSDK

--- app/src/main/java/com/muyi/numbers/MainActivity.java
+++ app/src/main/java/com/muyi/numbers/MainActivity.java
@@ -3,6 +3,8 @@ package com.muyi.numbers;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import cn.bmob.v3.Bmob;
+
 public class MainActivity extends BaseActivity {
 
     @Override
@@ -12,5 +14,18 @@ public class MainActivity extends BaseActivity {
         setTopBarColor(R.color.colorPrimary);
         Toolbar mToolbarTb = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbarTb);
+        Bmob.initialize(this, "01d88fc32ae11655cd0f1df65ce46fde");
+
+//        BmobQuery<Person> bmobQuery = new BmobQuery<Person>();
+//        bmobQuery.getObject("6b6c11c537", new >QueryListener<Person>() {
+//            @Override
+//            public void done(Person object,BmobException e) {
+//                if(e==null){
+//                    toast("查询成功");
+//                }else{
+//                    toast("查询失败：" + e.getMessage());
+//                }
+//            }
+//        });
     }
 }
